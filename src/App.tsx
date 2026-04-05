e6d28bbec408fe937d98aa199c0e63e6901be717
plumplum01 <plumplum010101@gmail.com>
2026-04-05 17:23:30 +0900

    Fix base path for custom domain plumyyy.com

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function Home() {
 
 export default function App() {
   return (
-    <BrowserRouter basename="/yoonseobin.github.io">
+    <BrowserRouter basename="/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
